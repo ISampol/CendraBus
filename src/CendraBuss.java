7657b1e7fab7e131a463dfe9b310de57c1636a43
ISampol <ismaelsampol@gmail.com>
2022-06-02 15:22:13 +0200

    Agregada ultima frase

--- src/CendraBuss.java
+++ src/CendraBuss.java
@@ -16,13 +16,15 @@ public class CendraBuss {
         System.out.println("Digues els CV de potencia");
         Caracteristiques.potencia = sc.next();
 
-        
+
 
         System.out.println("El vehicle marca "+ Caracteristiques.marcaDelConjunt + " té les següents característiques, " +
                 "combustible: " + Caracteristiques.combustible + ", " + Caracteristiques.nombreDeSeients + " seients, motor marca: " + Caracteristiques.marcaDelMotor +
                 " amb una cilindrada de " + Caracteristiques.cilindrada + " i " + Caracteristiques.potencia + " cavalls de potència. ");
 
         System.out.println("L’autobús té una longitud de xxx metres i será de color xxxx i incorporarà els següents extres xxxxxxx, xxxxx , xxxxx.");
+
+        System.out.println("Un cop acabat el vehicle tindrà un preu de xxxx euros i un pes de xxxx kgs.");
 
         sc.close();
     }
